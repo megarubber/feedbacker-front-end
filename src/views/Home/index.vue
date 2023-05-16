6b73fc0ce4d5b7ab887848b6ecc2b669727e4237
Guilherme Samuel <guisamuel800@gmail.com>
2023-05-15 23:47:00 -0300

    config all routes & views

---
+++ src/views/Home/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex justify-center py-10 bg-gray">
+        <p>feedbacker. All rights reserved</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
