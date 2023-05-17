dfbfcadb58b471ccfe37d9d4cce38c69cda54a48
Guilherme Samuel <guisamuel800@gmail.com>
2023-05-16 23:15:45 -0300

    config Home's index for header and contact components

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,11 +1,27 @@
 <template>
-    <div class="flex justify-center py-10 bg-gray">
-        <p>feedbacker. All rights reserved</p>
-    </div>
+  <CustomHeader />
+  <Contact />
+  <div class="flex justify-center py-10 bg-gray-200">
+    <p class="font-medium text-center">feedbacker. All rights reserved</p>
+  </div>
 </template>
 
 <script>
-export default {
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import Contact from './Contact.vue';
+import CustomHeader from './CustomHeader.vue';
 
+export default {
+  components: { Contact, CustomHeader },
+  setup () {
+    const router = useRouter();    
+      onMounted(() => {
+        const token = window.localStorage.getItem('token');
+        if(token) {
+          router.push({ name: 'Feedback' });
+        }
+    });
+  }
 }
 </script>
