150b7834d0972c81d8374f22c8ab66093b9bb40c
Guilherme Samuel <guisamuel800@gmail.com>
2023-05-16 21:41:03 -0300

    config postcss with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   presets: [],
   darkMode: 'media', // or 'class'
   theme: {
