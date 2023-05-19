207933d690aed280619561eff49572c59f564600
Guilherme Samuel <guisamuel800@gmail.com>
2023-05-19 02:44:25 +0000

    create new factories

---
+++ src/components/ModalFactory/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <teleport to="body">
+    <div
+      v-if="state.isActive"
+      @click="handleModalToggle({ status: false })"
+      class="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-50">
+      <div 
+        class="fixed mx-10"
+        :class="state.width"
+        @click.stop
+      >
+        <div class="flex flex-col overflow-hidden bg-white rounded-lg animate__animated animate__fadeInDown animate__faster">
+          <div class="flex flex-col px-12 py-10 bg-white">
+            <component is="ModalLogin" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<script>
+import { reactive, onMounted, onBeforeUnmount, defineAsyncComponent } from 'vue';
+import useModal from '../../hooks/useModal';
+const DEFAULT_WIDTH = 'w-3/4 lg:w-1/3';
+
+const ModalLogin = defineAsyncComponent(() => import('../ModalLogin/index.vue'));
+
+export default {
+  setup() {
+    const modal = useModal();
+    const state = reactive({
+      isActive: false,
+      component: {},
+      props: {},
+      width: DEFAULT_WIDTH
+    });
+    
+    onMounted(() => {
+      modal.listen(handleModalToggle);
+    });
+    
+    onBeforeUnmount(() => {
+      modal.off(handleModalToggle);
+    });
+
+    function handleModalToggle(payload) {
+      if(payload.status) {
+        state.component = payload.component;
+        state.props = payload.props;
+        state.width = payload.width ?? DEFAULT_WIDTH;
+      } else {
+        state.component = {};
+        state.props = {};
+        state.width = DEFAULT_WIDTH;
+      }
+      state.isActive = payload.status;
+    }
+
+    return {
+      state,
+      handleModalToggle
+    }
+  }
+}
+</script>
---
+++ src/components/ModalLogin/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Teste</h1>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+</style>
