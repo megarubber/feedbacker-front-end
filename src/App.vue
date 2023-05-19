fa5e5a6fde57a24907d9a41f6cfcaa534a18a24f
Guilherme Samuel <guisamuel800@gmail.com>
2023-05-19 02:46:51 +0000

    start the modal factory in main view

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
+  <ModalFactory />
   <RouterView />
 </template>
+
+<script>
+import ModalFactory from './components/ModalFactory/index.vue';
+export default {
+  components: {
+    ModalFactory
+  }
+}
+</script>
