07dea41f7433c237d600be304bc9f4d7954d1fd3
Guilherme Samuel <guisamuel800@gmail.com>
2023-05-20 22:44:36 +0000

    chore: add vue-toastification to dependencies

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Toast, { POSITION } from 'vue-toastification'
 import router from './router'
 import '@/assets/css/tailwind.css'
 import 'animate.css';
+import 'vue-toastification/dist/index.css';
 
 const app = createApp(App)
-
 app.use(router)
-
+app.use(Toast, { position: POSITION.BOTTOM_RIGHT });
 app.mount('#app')
