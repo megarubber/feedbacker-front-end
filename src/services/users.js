d964a4c6480688bbbd1acecfa2480302d9b46ab6
Guilherme Samuel <guisamuel800@gmail.com>
2023-05-26 00:27:33 +0000

    fix users syntax

--- src/services/users.js
+++ src/services/users.js
@@ -1,9 +1,8 @@
 export default httpClient => ({
   getMe: async () => {
     const response = await httpClient.get('/users/me');
-  };
-
-  return {
-    data: response.data
-  };
+    return {
+      data: response.data
+    };
+  },
 });
